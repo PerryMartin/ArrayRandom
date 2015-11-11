82f6328742868da1f336c16896a61b407a337ba4
PerryMartin <perry.martin@ocsbstudent.ca>
2015-11-11 14:10:57 -0500

    v1.0

---
+++ ArrayRandom/Array.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ArrayRandom
+{
+    partial class frmArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstRanBox = new System.Windows.Forms.ListBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstRanBox
+            // 
+            this.lstRanBox.FormattingEnabled = true;
+            this.lstRanBox.Location = new System.Drawing.Point(212, 13);
+            this.lstRanBox.Name = "lstRanBox";
+            this.lstRanBox.Size = new System.Drawing.Size(145, 186);
+            this.lstRanBox.TabIndex = 0;
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Location = new System.Drawing.Point(13, 13);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(0, 13);
+            this.lblQuestion.TabIndex = 1;
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(16, 128);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(174, 71);
+            this.btnRoll.TabIndex = 2;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // frmArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 211);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.lstRanBox);
+            this.Name = "frmArray";
+            this.Text = "Arrays";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstRanBox;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Button btnRoll;
+    }
+}
+
---
+++ ArrayRandom/Array.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Created by: Perry Martin
+ * Created on: 11-11-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit#5-01
+ * This program displays an example of a array
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayRandom
+{
+    public partial class frmArray : Form
+    {
+
+        // global variables
+        int[] arrayOfNumbers = new int[10];
+
+      
+        public frmArray()
+        {
+        
+
+            InitializeComponent();
+        }
+
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            this.lstRanBox.Items.Clear();
+
+
+            // Varribles
+            int loopCounter = 0;
+            int average = 0;
+            int listAdder;
+            int randomNumbers;
+
+            Random rnd = new Random();
+
+            while (loopCounter < 10)
+            {
+
+
+                randomNumbers = rnd.Next(1, 11);
+                arrayOfNumbers[loopCounter] = randomNumbers;
+                listAdder = arrayOfNumbers[loopCounter];
+                this.lstRanBox.Items.Add(listAdder);
+                average = average + arrayOfNumbers[loopCounter];
+                loopCounter++;
+            }
+                average = average / 10;
+                this.lblQuestion.Text = "this is the average: " + average;
+            
+        }
+     }
+}
